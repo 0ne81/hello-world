04f0bb519d55d84fa53f915abad6a633344e127d
heese <heese@192.168.219.198>
2020-11-23 22:50:55 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hi");
+		System.out.println("99");
+		System.out.println("81" + " and\nnot " + "희승");
+	}
+}
